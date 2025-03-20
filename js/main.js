f6d7255f68219356a6235b2d4477d255355ce9c3
Antonio Ponce <desarrollowebaps@gmail.com>
2025-03-19 21:44:45 -0400

    22curriculumNuevo__bloqueB

--- js/main.js
+++ js/main.js
@@ -16,6 +16,18 @@ const mostrarFuncionPrincipal = async () =>{
         const res = await data.json() 
         pintarExpConecta(res)     
 
+        document.addEventListener("click",(e)=>{
+            if(e.target.id === "fecha4"){
+                console.log(" Soy fecha4 ")
+            }else if(e.target.id === "fecha3"){
+                console.log(" Soy fecha3 ")
+            }else if(e.target.id === "fecha2"){
+                console.log(" Soy fecha2 ")
+            }else if(e.target.id === "fecha1"){
+                console.log(" Soy fecha1 ")
+            }
+        })
+
 
 
     }catch(error){
@@ -25,18 +37,117 @@ const mostrarFuncionPrincipal = async () =>{
 
 
 
-const pintarExpConecta = (res) =>{
+
+const pintarInvertronic = (res) =>{
     console.log(res)
-    
-    contTemplateExp.querySelector("#title1").textContent = res[0].title1
-    contTemplateExp.querySelector("#text1").textContent = res[0].description1
-    contTemplateExp.querySelector("#textWeb").textContent = res[0].sitioWeb
-    contTemplateExp.querySelector("#textWeb").setAttribute("href",res[0].sitioWeb)            
+        
+    contTemplateExp.querySelector("#title1").textContent = res[4].title1
+    contTemplateExp.querySelector("#title2").textContent = res[4].title2
+    contTemplateExp.querySelector("#title3").textContent = res[4].title3
+    contTemplateExp.querySelector("#title4").textContent = res[4].title4
 
+    contTemplateExp.querySelector("#text1").textContent = res[4].description1
+    contTemplateExp.querySelector("#text2").textContent = res[4].description2
+    contTemplateExp.querySelector("#text3").textContent = res[4].description3
+    contTemplateExp.querySelector("#text4").textContent = res[4].description4
 
+    contTemplateExp.querySelector("#textWeb").textContent = res[4].sitioWeb
+    contTemplateExp.querySelector("#textWeb").setAttribute("href",res[4].sitioWeb)            
 
+    //Bases apendChild
     contFragmentExp.appendChild(contTemplateExp)
     contBaseExp.appendChild(contFragmentExp)
 
 }
 
+
+const pintarSTProvin = (res) =>{
+    console.log(res)
+        
+    contTemplateExp.querySelector("#title1").textContent = res[3].title1
+    contTemplateExp.querySelector("#title2").textContent = res[3].title2
+    contTemplateExp.querySelector("#title3").textContent = res[3].title3
+    contTemplateExp.querySelector("#title4").textContent = res[3].title4
+
+    contTemplateExp.querySelector("#text1").textContent = res[3].description1
+    contTemplateExp.querySelector("#text2").textContent = res[3].description2
+    contTemplateExp.querySelector("#text3").textContent = res[3].description3
+    contTemplateExp.querySelector("#text4").textContent = res[3].description4
+
+    contTemplateExp.querySelector("#textWeb").textContent = res[3].sitioWeb
+    contTemplateExp.querySelector("#textWeb").setAttribute("href",res[3].sitioWeb)            
+
+    //Bases apendChild
+    contFragmentExp.appendChild(contTemplateExp)
+    contBaseExp.appendChild(contFragmentExp)
+
+}
+
+
+const pintarSTLive = (res) =>{
+    console.log(res)
+        
+    contTemplateExp.querySelector("#title1").textContent = res[2].title1
+    contTemplateExp.querySelector("#title2").textContent = res[2].title2
+    contTemplateExp.querySelector("#title3").textContent = res[2].title3
+    contTemplateExp.querySelector("#title4").textContent = res[2].title4
+
+    contTemplateExp.querySelector("#text1").textContent = res[2].description1
+    contTemplateExp.querySelector("#text2").textContent = res[2].description2
+    contTemplateExp.querySelector("#text3").textContent = res[2].description3
+    contTemplateExp.querySelector("#text4").textContent = res[2].description4
+
+    contTemplateExp.querySelector("#textWeb").textContent = res[2].sitioWeb
+    contTemplateExp.querySelector("#textWeb").setAttribute("href",res[2].sitioWeb)            
+
+    //Bases apendChild
+    contFragmentExp.appendChild(contTemplateExp)
+    contBaseExp.appendChild(contFragmentExp)
+
+}
+
+
+const pintarFrontend = (res) =>{
+    console.log(res)
+        
+    contTemplateExp.querySelector("#title1").textContent = res[1].title1
+    contTemplateExp.querySelector("#title2").textContent = res[1].title2
+    contTemplateExp.querySelector("#title3").textContent = res[1].title3
+    contTemplateExp.querySelector("#title4").textContent = res[1].title4
+
+    contTemplateExp.querySelector("#text1").textContent = res[1].description1
+    contTemplateExp.querySelector("#text2").textContent = res[1].description2
+    contTemplateExp.querySelector("#text3").textContent = res[1].description3
+    contTemplateExp.querySelector("#text4").textContent = res[1].description4
+
+    contTemplateExp.querySelector("#textWeb").textContent = res[1].sitioWeb
+    contTemplateExp.querySelector("#textWeb").setAttribute("href",res[1].sitioWeb)            
+
+    //Bases apendChild
+    contFragmentExp.appendChild(contTemplateExp)
+    contBaseExp.appendChild(contFragmentExp)
+
+}
+
+
+const pintarExpConecta = (res) =>{
+    console.log(res)
+        
+    contTemplateExp.querySelector("#title1").textContent = res[0].title1
+    contTemplateExp.querySelector("#title2").textContent = res[0].title2
+    contTemplateExp.querySelector("#title3").textContent = res[0].title3
+    contTemplateExp.querySelector("#title4").textContent = res[0].title4
+
+    contTemplateExp.querySelector("#text1").textContent = res[0].description1
+    contTemplateExp.querySelector("#text2").textContent = res[0].description2
+    contTemplateExp.querySelector("#text3").textContent = res[0].description3
+    contTemplateExp.querySelector("#text4").textContent = res[0].description4
+
+    contTemplateExp.querySelector("#textWeb").textContent = res[0].sitioWeb
+    contTemplateExp.querySelector("#textWeb").setAttribute("href",res[0].sitioWeb)            
+
+    //Bases apendChild
+    contFragmentExp.appendChild(contTemplateExp)
+    contBaseExp.appendChild(contFragmentExp)
+
+}
