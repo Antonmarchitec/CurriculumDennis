ae3c63a49524ce20934a34366fca91b32da77942
Dennis Ponce <desarrollowebaps@gmail.com>
2026-05-24 02:22:37 -0400

    99_ActualizandoMediaQueries

--- js/fechas.js
+++ js/fechas.js
@@ -10,10 +10,22 @@ function mostrarScroll(elemento, scroll, clase){
     });
 }
 
+//FUNCION MOSTRAR ELEMEMTO CON SCROLL
+function togglePanel(boton, elemento, clase){
+    boton.addEventListener("click", () => {
+        elemento.classList.toggle(clase);
+    });
+}
 
+
+
+
+//////////////////////
 
 const cajaFechas = document.getElementById("cajaFechas");
 mostrarScroll(cajaFechas, 2000, "mostrar");
 
 
-
+let btnProyectos = document.getElementById("btnProyectos")
+let contProyectos__block = document.getElementById("contProyectos__block")
+togglePanel(btnProyectos, contProyectos__block, "contProyectos__block_JS")
